cd6889b66a69bab83128bb921551d5c9be5b4cab
Akash Nara <akash.smartsenses@gmail.com>
2016-08-09 12:16:09 +0530

    Nt First github Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by smartSense on 01/08/16.
+//  Copyright © 2016 smartSense. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        "hello how  are you"
+        "hello how  are you"
+        "hello how  are you"
+        "hello how  are you"
+        "hello how  are you"
+        "hello how  are you"
+        "hello how  are you"
+        "hello how  are you"
+        "hello how  are you"
+
+        "yes o got it  from news line"
+        "hello how  are you"
+"gdgdfggdhdfhfhfdhhhdhfhdhdhdfhhhdhfhdhhdhdhdfhdhhdhdhfhhhdfhfhhhhh"
+        
+
+        "yes i knows what is going on"
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
